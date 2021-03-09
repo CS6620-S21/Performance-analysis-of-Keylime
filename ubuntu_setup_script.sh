d5ed56263cadb529fb4f35ebcbc8360c4856d0b6
Yilin Ma <31215089+MartinMa28@users.noreply.github.com>
2021-03-09 01:01:32 -0800

    Change tpm2-tss from 2.4.5 to 3.0.3 in Ubuntu 20.04 script.

--- ubuntu_setup_script.sh
+++ ubuntu_setup_script.sh
@@ -5,7 +5,7 @@ apt-get upgrade
 
 # install keylime v6.0.1
 apt-get install -y wget gcc make vim python3-pip
-apt-get install -y build-essential libssl-dev swig python3-dev libtss2-dev
+apt-get install -y build-essential libssl-dev swig python3-dev
 cd /root
 wget https://github.com/keylime/keylime/archive/6.0.1.tar.gz
 tar -zxvf 6.0.1.tar.gz
@@ -18,10 +18,10 @@ cd /root
 wget https://github.com/stefanberger/libtpms/archive/v0.7.7.tar.gz
 tar -zxvf v0.7.7.tar.gz
 cd libtpms-0.7.7/
-sudo apt-get -y install automake autoconf libtool gcc build-essential libssl-dev dh-exec pkg-config
+apt-get -y install automake autoconf libtool gcc build-essential libssl-dev dh-exec pkg-config gawk
 ./autogen.sh --with-openssl
 make dist
-dpkg-buildpackage -us -uc -j4
+dpkg-buildpackage -us -uc -j$(nproc)
 cd ..
 dpkg -i libtpms0_0.7.7_amd64.deb libtpms-dev_0.7.7_amd64.deb
 
@@ -31,11 +31,70 @@ wget https://github.com/stefanberger/swtpm/archive/v0.5.2.tar.gz
 tar -zxvf v0.5.2.tar.gz
 cd swtpm-0.5.2/
 apt-get -y install  libfuse-dev libglib2.0-dev libgmp-dev expect libtasn1-dev socat tpm-tools python3-twisted gnutls-dev gnutls-bin net-tools softhsm2 libseccomp-dev
-./autogen.sh --with-openssl
-make dist
-dpkg-buildpackage -us -uc -j4
+dpkg-buildpackage -us -uc -j$(nproc)
 cd ..
 dpkg -i swtpm-dev_0.5.2_amd64.deb swtpm-libs_0.5.2_amd64.deb swtpm-tools_0.5.2_amd64.deb swtpm_0.5.2_amd64.deb
 
 # install dbus
 apt-get install dbus
+
+# install tpm2-tss dependencies
+apt-get -y install \
+  autoconf-archive \
+  libcmocka0 \
+  libcmocka-dev \
+  procps \
+  iproute2 \
+  build-essential \
+  git \
+  pkg-config \
+  gcc \
+  libtool \
+  automake \
+  libssl-dev \
+  uthash-dev \
+  autoconf \
+  doxygen \
+  libjson-c-dev \
+  libini-config-dev \
+  libcurl4-gnutls-dev
+
+# install tpm2-tss
+cd /root
+wget https://github.com/tpm2-software/tpm2-tss/releases/download/3.0.3/tpm2-tss-3.0.3.tar.gz
+tar -zxvf tpm2-tss-3.0.3.tar.gz
+cd tpm2-tss-3.0.3
+useradd --system --user-group tss
+./configure --prefix=/usr
+make -j$(nproc)
+make install
+
+# install tpm2-tools
+cd /root
+apt-get install -y uuid uuid-dev
+wget https://github.com/tpm2-software/tpm2-tools/releases/download/4.3.0/tpm2-tools-4.3.0.tar.gz
+tar -zxvf tpm2-tools-4.3.0.tar.gz
+cd tpm2-tools-4.3.0
+./configure --prefix=/usr/local
+make -j$(nproc)
+make install
+
+# install tpm2-abrmd
+cd /root
+wget https://github.com/tpm2-software/tpm2-abrmd/releases/download/2.3.3/tpm2-abrmd-2.3.3.tar.gz
+tar -zxvf tpm2-abrmd-2.3.3.tar.gz
+cd tpm2-abrmd-2.3.3
+apt-get install -y libglib2.0-dev
+./configure --with-dbuspolicydir=/etc/dbus-1/system.d \
+            --with-systemdsystemunitdir=/lib/systemd/system \
+            --with-systemdpresetdir=/lib/systemd/system-preset \
+            --datarootdir=/usr/share
+make -j$(nproc)
+make install
+
+# fix bugs to run webapp
+cd /root/keylime-6.0.1/keylime
+cp -r ./static /usr/local/lib/python3.8/dist-packages/keylime
+
+# https://github.com/keylime/keylime/issues/519#issuecomment-778323883
+# make the code change in the link
